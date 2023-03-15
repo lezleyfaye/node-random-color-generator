dc3c6c0901338bae8898496b7e1f95bf4e62e3a2
Lezley Faye <lezleydavidson@gmail.com>
2023-03-15 12:50:15 +0100

    Adding last chagnes

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
-// console.log('######');
+//console.log fpr #'s, need a object for other part??
 
-console.log(randomColor());
-console.log(chalk(randomColor));
+//const hash = '#';
+//const space = ' ';
+//console.log(hash.repeat(31));
+//console.log(hash.repeat(5) + space.repeat(21) + hash.repeat(5));
+//console.log(hash.repeat(31));
+
